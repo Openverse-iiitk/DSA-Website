59fa223fdac5414c7956420c1010974dc88b7d5c
Nikhil A <nikhilanil8721@gmail.com>
2025-05-10 12:27:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import LinkedListVisualizer from './components/LinkedListVisualizer'
 import DiySection from './components/DiySection'
 import DoublyLinkedListExplanation from './components/DoublyLinkedListExplanation'
 import { generateCppCode } from './utils/codeGenerator'
+import Split from 'react-split'
 
 function App() {
   const [nodes, setNodes] = useState([]);
   const [code, setCode] = useState(generateCppCode([]));
 
-  // Function to update both nodes and code
   const updateNodesAndCode = (newNodes) => {
     setNodes(newNodes);
     setCode(generateCppCode(newNodes));
@@ -21,20 +21,25 @@ function App() {
       <header className="app-header">
         <h1>C++ Doubly Linked List Visualization</h1>
       </header>
-      <div className="split-view">
-        <div className="panel panel-left">
-          <h2>C++ Implementation</h2>
-          <CodeViewer code={code} />
-        </div>
-        <div className="panel panel-right">
-          <h2>Interactive Visualization</h2>
-          <LinkedListVisualizer 
-            nodes={nodes} 
-            onNodesChange={updateNodesAndCode} 
-          />
-          <DoublyLinkedListExplanation />
-          <DiySection code={code} />
-        </div>
+
+      <div className="split-container">
+        <Split
+          className="split-view"
+          sizes={[50, 50]}
+          minSize={200}
+          gutterSize={10}
+        >
+          <div className="panel panel-left">
+            <h2>C++ Implementation</h2>
+            <CodeViewer code={code} />
+          </div>
+          <div className="panel panel-right">
+            <h2>Interactive Visualization</h2>
+            <LinkedListVisualizer nodes={nodes} onNodesChange={updateNodesAndCode} />
+            <DoublyLinkedListExplanation />
+            <DiySection code={code} />
+          </div>
+        </Split>
       </div>
     </div>
   )
